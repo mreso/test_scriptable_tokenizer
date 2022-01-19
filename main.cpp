562726c785d410fb1b63d3f2ca4b63b723375556
Matthias Reso <13337103+mreso@users.noreply.github.com>
2022-01-19 18:44:54 +0000

    Adopted workaround of https://github.com/pytorch/text/issues/1255

--- main.cpp
+++ main.cpp
@@ -1,7 +1,37 @@
-#include <torch/torch.h>
+// #include <torch/torch.h>
+// #include <torch/script.h>
+
+// int main(void) {
+//     auto tokenizer = torch::jit::load("../tokenizer.pt");
+
+// }
+
 #include <torch/script.h>
+#include <torch/nn/functional/activation.h>
 
-int main(void) {
-    auto tokenizer = torch::jit::load("../tokenizer.pt");
+#include <iostream>
+#include <vector>
+#include <string>
 
+using namespace std;
+
+int main(int argc, const char* argv[]) {
+
+    std::cout << "Loading model...\n";
+
+    torch::jit::script::Module module;
+    try {
+        module = torch::jit::load(argv[1]);
+    } catch (const c10::Error& e) {
+        return -1;
+    }
+
+    torch::NoGradGuard no_grad; // ensures that autograd is off
+
+    namespace F = torch::nn::functional;
+    
+    torch::jit::IValue tokens_ivalue = module.forward(std::vector<c10::IValue>(1,"test@gmail.com 00000001"));
+    std::cout << "result " << tokens_ivalue << '\n';
+
+    return 0;
 }
--- test_tokenizers.py
+++ test_tokenizers.py
@@ -1,22 +1,28 @@
 
 
-from torchtext import transforms
+# from torchtext import transforms
+# import torch
+
+
+# sentence = "The green grasshopper jumped over the fence"
+
+
+# encoder_json = "gpt2_bpe_encoder.json"
+# bpe_vocab = "gpt2_bpe_vocab.bpe"
+# tokenizer = transforms.GPT2BPETokenizer(
+#     encoder_json_path=encoder_json,
+#     vocab_bpe_path=bpe_vocab)
+
+# tokenizer = torch.jit.script(tokenizer)
+
+# torch.jit.save(tokenizer, 'tokenizer.pt')
+
+# t = torch.jit.load('tokenizer.pt')
+
+# print(t(sentence))
+
 import torch
-
-
-sentence = "The green grasshopper jumped over the fence"
-
-
-encoder_json = "gpt2_bpe_encoder.json"
-bpe_vocab = "gpt2_bpe_vocab.bpe"
-tokenizer = transforms.GPT2BPETokenizer(
-    encoder_json_path=encoder_json,
-    vocab_bpe_path=bpe_vocab)
-
-tokenizer = torch.jit.script(tokenizer)
-
-torch.jit.save(tokenizer, 'tokenizer.pt')
-
-t = torch.jit.load('tokenizer.pt')
-
-print(t(sentence))
+from torchtext.experimental.transforms import regex_tokenizer
+tokenizer = regex_tokenizer([])
+tokenizer_scripted = torch.jit.script(tokenizer)
+tokenizer_scripted.save("tokenizer.pt")
